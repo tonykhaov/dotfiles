29b2fe2cc9eefee8f63cccd88fe3e67fcef0c4b3
Tony Khaov <tony.khaov@gmail.com>
2021-05-28 14:55:13 +0200

    chore: import vscode.sh for extensions, snippets and settings

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ TODO:
 
 - [x] import setup.sh to automatically install brew, nvm, zsh, etc.
 - [x] import mas.sh to automatically download mac app store apps
+- [x] import vscode.sh to automatically download extensions, set snippets and settings
 - [x] import Terminal.app theme
-- [ ] import VS code configs
---
+++ vscode.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+
+# Install VScode extensions
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension christian-kohler.path-intellisense
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension GitHub.github-vscode-theme
+code --install-extension GitHub.vscode-pull-request-github
+code --install-extension heybourn.headwind
+code --install-extension jasonnutter.search-node-modules
+code --install-extension KnisterPeter.vscode-commitizen
+code --install-extension leizongmin.node-module-intellisense
+code --install-extension mhutchie.git-graph
+code --install-extension ms-vscode.atom-keybindings
+code --install-extension naumovs.color-highlight
+code --install-extension PKief.material-icon-theme
+code --install-extension ritwickdey.LiveServer
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vscodevim.vim
+
+# import snippets, keybindings and settings to vscode
+chmod u+w+r vscode
+cp -r ./vscode/* ~/Library/Application\ Support/Code/User/ 2> /dev/null
