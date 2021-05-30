3bf5ff4e079f473b2ce140bad12188cb8bb41b0a
Tony Khaov <tony.khaov@gmail.com>
2021-05-30 15:27:04 +0200

    chore: add more apps and yarn to brew.sh

--- brew.sh
+++ brew.sh
@@ -1,23 +1,27 @@
 # Install brew additional formulae
 brew install ffmpeg
-# brew install gs
-# brew install imagemagick
+brew install gs
+brew install imagemagick
+brew install pnpm
+brew install trash
+brew install yarn
 brew install youtube-dl 
 
 # Install Homebrew casks
 brew install --cask 1password
 brew install --cask alfred
 brew install --cask anki
-# brew install --cask bettertouchtool
+brew install --cask appcleaner
+brew install --cask bettertouchtool
 brew install --cask brave-browser
-# brew install --cask calibre
+brew install --cask calibre
 brew install --cask discord
 brew install --cask docker
 brew install --cask figma
 # brew install --cask hazel
 brew install --cask qbittorrent
 # brew install --cask maccy
-# brew install --cask molotov
+brew install --cask molotov
 # brew install --cask muzzle
 brew install --cask nordvpn
 brew install --cask notion
@@ -27,7 +31,8 @@ brew install --cask rectangle
 brew install --cask spotify
 brew install --cask the-unarchiver
 # brew install --cask transmit
-# brew install --cask turbo-boost-switcher # Only for Intel chipsets
+# brew install --cask turbo-boost-switcher-pro # Only for Intel chipsets
 # brew install --cask typora
+brew install --cask vanilla
 brew install --cask visual-studio-code
 brew install --cask vlc
