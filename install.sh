ce5a749d306a64e3b55a8362eee6950714b4275b
Tony Khaov <tony.khaov@gmail.com>
2021-06-02 22:27:27 +0200

    chore: use automated shell script for alfred settings

--- install.sh
+++ install.sh
@@ -1,26 +1,23 @@
 #! /usr/bin/env bash
 
 # setup.sh
-chmod u+x ./setup.sh
-./setup.sh
+chmod u+x ./setup.sh && ./setup.sh
 
 # mas.sh
-chmod u+x ./mas.sh
-./mas.sh
+chmod u+x ./mas.sh && ./mas.sh
 
 # brew.sh
-chmod u+x ./brew.sh
-./brew.sh
+chmod u+x ./brew.sh && ./brew.sh
 
 # vscode.sh
-chmod u+x ./vscode.sh
-./vscode.sh
+chmod u+x ./vscode.sh && ./vscode.sh
 
 # ssh.sh
-chmod u+x ./ssh.sh
-./ssh.sh
+chmod u+x ./ssh.sh && ./ssh.sh
 
 # TODO: prompt the user if he wants these dir
 # stock and lab
-chmod u+x ./easy-dev.sh
-./easy-dev.sh
+chmod u+x ./easy-dev.sh && ./easy-dev.sh
+
+# alfred.sh
+chmod u+x ./alfred.sh && ./alfred.sh
