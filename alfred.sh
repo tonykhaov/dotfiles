348e5c4c14aff9467c3fa5d6f52c4148c0c6b515
Tony Khaov <tony.khaov@gmail.com>
2021-06-03 21:27:20 +0200

    chore: update alfred custom searches

--- alfred.sh
+++ alfred.sh
@@ -1,16 +1,16 @@
 #! /usr/bin/env bash
 
 # Custom Searches
-open alfred://customsearch/Cambridge%20dictionary%20for%3A%20%7Bquery%7D/en/utf8/nospace/https%3A%2F%2Fdictionary.cambridge.org%2Fdictionary%2Fenglish%2F%7Bquery%7D%3Fq%3D
-open alfred://customsearch/Espanol%20%3A%20%7Bquery%7D/es/utf8/nospace/https%3A%2F%2Fdle.rae.es%2F%7Bquery%7D
-open alfred://customsearch/TV5%20%3A%20%7Bquery%7D/fr/utf8/nospace/https%3A%2F%2Flangue-francaise.tv5monde.com%2Fdecouvrir%2Fdictionnaire%2Fe%2F%7Bquery%7D
-open alfred://customsearch/Japanese%20for%3A%20%7Bquery%7D/ja/utf8/nospace/http%3A%2F%2Fwww.romajidesu.com%2Fdictionary%2Fmeaning-of-%7Bquery%7D.html
-open alfred://customsearch/Synonymes%20pour%20%3A%20%7Bquery%7D/sy/utf8/nospace/https%3A%2F%2Fcrisco2.unicaen.fr%2Fdes%2Fsynonymes%2F%7Bquery%7D
+open alfred://customsearch/%5BEN%5D%20Meaning%3A%20%7Bquery%7D/en/utf8/nospace/https%3A%2F%2Fdictionary.cambridge.org%2Fdictionary%2Fenglish%2F%7Bquery%7D
+open alfred://customsearch/%5BES%5D%20Definicion%3A%20%7Bquery%7D/es/utf8/nospace/https%3A%2F%2Fdle.rae.es%2F%7Bquery%7D
+open alfred://customsearch/%5BFR%5D%20Definition%20%3A%20%7Bquery%7D/fr/utf8/nospace/https%3A%2F%2Flangue-francaise.tv5monde.com%2Fdecouvrir%2Fdictionnaire%2Fe%2F%7Bquery%7D
+open alfred://customsearch/%5BJAP%5D%20Meaning%3A%20%7Bquery%7D/ja/utf8/nospace/http%3A%2F%2Fwww.romajidesu.com%2Fdictionary%2Fmeaning-of-%7Bquery%7D.html
+open alfred://customsearch/Crisco%20%3A%20%7Bquery%7D/sy/utf8/nospace/https%3A%2F%2Fcrisco2.unicaen.fr%2Fdes%2Fsynonymes%2F%7Bquery%7D
 open alfred://customsearch/Thesaurus%3A%20%7Bquery%7D/syn/utf8/nospace/https%3A%2F%2Fwww.thesaurus.com%2Fbrowse%2F%7Bquery%7D%3Fs%3Dt
 open alfred://customsearch/Temp%20Mail/tempmail/utf8/nospace/https%3A%2F%2Ftemp-mail.org%2F
 
 # Workflows
-./alfred/audio.alfredworkflow
-./alfred/bear.alfredworkflow
-./alfred/bookmark.alfredworkflow
-./alfred/reminder.alfredworkflow
+open -a "Alfred 4" ./alfred/audio.alfredworkflow
+open -a "Alfred 4" ./alfred/bear.alfredworkflow
+open -a "Alfred 4" ./alfred/bookmark.alfredworkflow
+open -a "Alfred 4" ./alfred/reminder.alfredworkflow
