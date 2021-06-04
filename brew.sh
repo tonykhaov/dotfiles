a520d8642f1d26a7781da6a61d9287c52f99fda9
Tony Khaov <tony.khaov@gmail.com>
2021-06-04 11:58:53 +0200

    chore: opt out some brew formulaes and remove some too

--- brew.sh
+++ brew.sh
@@ -1,41 +1,31 @@
 # Install brew additional formulae
 brew install eslint
-brew install ffmpeg
-brew install gs
-brew install imagemagick
+# brew install ffmpeg gs imagemagick
 brew install prettier
-brew install pnpm
 brew install trash
 brew install yarn
 brew install youtube-dl 
 
 # Install Homebrew casks
 brew install --cask 1password
+# brew install --cask adobe-creative-cloud
 brew install --cask alfred
 brew install --cask anki
 brew install --cask appcleaner
-brew install --cask bettertouchtool
 brew install --cask brave-browser
 brew install --cask calibre
 brew install --cask discord
 brew install --cask docker
 brew install --cask figma
-# brew install --cask hazel
 brew install --cask qbittorrent
-# brew install --cask maccy
+# brew install --cask microsoft-teams
 brew install --cask molotov
-# brew install --cask muzzle
 brew install --cask nordvpn
 brew install --cask notion
-# brew install --cask numi
 brew install --cask pomotroid
 brew install --cask postman
 brew install --cask rectangle
 brew install --cask spotify
-brew install --cask the-unarchiver
-# brew install --cask transmit
 # brew install --cask turbo-boost-switcher-pro # Only for Intel chipsets
-# brew install --cask typora
-brew install --cask vanilla
 brew install --cask visual-studio-code
 brew install --cask vlc
