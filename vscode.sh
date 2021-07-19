48dbdcbdb1de0be6ff9fb6473af952400c6dcff2
Tony Khaov <tony.khaov@gmail.com>
2021-07-19 18:30:42 +0200

    chore: add new vscode extensions

--- vscode.sh
+++ vscode.sh
@@ -7,6 +7,7 @@ code --install-extension christian-kohler.path-intellisense
 code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
+code --install-extension EditorConfig.EditorConfig
 code --install-extension esbenp.prettier-vscode
 code --install-extension formulahendry.auto-rename-tag
 code --install-extension GitHub.github-vscode-theme
@@ -16,14 +17,16 @@ code --install-extension jasonnutter.search-node-modules
 code --install-extension KnisterPeter.vscode-commitizen
 code --install-extension leizongmin.node-module-intellisense
 code --install-extension mhutchie.git-graph
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-vscode.atom-keybindings
 code --install-extension naumovs.color-highlight
+code --install-extension patbenatar.advanced-new-file
 code --install-extension PKief.material-icon-theme
 code --install-extension ritwickdey.LiveServer
+code --install-extension sleistner.vscode-fileutils
 code --install-extension VisualStudioExptTeam.vscodeintellicode
 code --install-extension vscodevim.vim
-code --install-extension patbenatar.advanced-new-file
-code --install-extension sleistner.vscode-fileutils
 
 # Import snippets, keybindings and settings to vscode
 chmod u+w+r vscode
