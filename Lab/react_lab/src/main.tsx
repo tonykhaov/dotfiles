89efed5325f6e8f9f468b7bbd59c41b18b478e77
Tony Khaov <tony.khaov@gmail.com>
2021-08-04 11:32:38 +0200

    chore: update react_lab dependencies

--- Lab/react_lab/src/main.tsx
+++ Lab/react_lab/src/main.tsx
@@ -2,12 +2,12 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import 'tailwindcss/tailwind.css'
 import AppProviders from './context/app-providers'
-import App from './components/App'
+import App from './App'
 import { worker } from './mocks/browser'
 
-if (process.env.NODE_ENV === 'development') {
-  worker.start()
-}
+// if (process.env.NODE_ENV === 'development') {
+//   worker.start()
+// }
 
 ReactDOM.render(
   <React.StrictMode>
