49597960a8d420a3d3e5bcc6e66cf8843f42e778
Tony Khaov <tony.khaov@gmail.com>
2021-08-19 16:49:53 +0200

    chore: install mac apps iina, imageoptim, dropzone

--- brew.sh
+++ brew.sh
@@ -11,17 +11,19 @@ brew install --cask anki
 brew install --cask appcleaner
 brew install --cask brave-browser
 brew install --cask calibre
+brew install --cask cheatsheet
 brew install --cask discord
 brew install --cask docker
+brew install --cask dropzone
 brew install --cask figma
+brew install --cask iina
+brew install --cask imageoptim
 brew install --cask qbittorrent
 # brew install --cask microsoft-teams
 brew install --cask molotov
 brew install --cask nordvpn
 brew install --cask notion
-brew install --cask pomotroid
 brew install --cask postman
 brew install --cask rectangle
 brew install --cask spotify
 brew install --cask visual-studio-code
-brew install --cask vlc
