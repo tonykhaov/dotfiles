f0e3b40d62e35bda9df8f5bf58290786d7f9225a
Tony Khaov <tony.khaov@gmail.com>
2022-04-29 10:24:08 +0200

    feat: update react_lab

--- Lab/react_lab/src/main.tsx
+++ Lab/react_lab/src/main.tsx
@@ -1,21 +1,23 @@
 import * as React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import AppProviders from '@src/context/app-providers'
 import App from '@src/App'
 import 'uno.css'
 import '@unocss/reset/tailwind.css'
-
 import { worker } from '@src/utils/mocks/browser'
 
 if (process.env.NODE_ENV === 'development') {
-  worker.start()
+  // UNCOMMENT IF YOU WANT TO MOCK YOUR REQUESTS IN DEVELOPMENT MODE
+  // worker.start()
 }
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLElement
+const root = createRoot(container)
+
+root.render(
   <React.StrictMode>
     <AppProviders>
       <App />
     </AppProviders>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
