45533bc2015bf1c049c3b636e5bea2f7c92c9155
Tony Khaov <tony.khaov@gmail.com>
2022-12-06 11:43:43 +0100

    Revert "mas > add toggl track"
    This reverts commit 2a0c6647d36824f42914dc091f8cb1eb26c77490.

--- mas.sh
+++ mas.sh
@@ -9,16 +9,15 @@ mas install 1287239339 # ColorSlurp
 mas install 975937182 # Fantastical - Calendar & Tasks
 mas install 1444383602 # GoodNotes 5
 mas install 430798174 # HazeOver • Distraction Dimmer™
-mas install 288545208 # Instapaper
 mas install 409183694 # Keynote
 mas install 409203825 # Numbers
 mas install 360593530  # Notability
 mas install 409201541 # Pages
+mas install 568494494 # Pocket
 mas install 1233965871 # ScreenBrush
 mas install 1176895641 # Spark - Email App By Readdle
 mas install 1528890965 # TextSniper - OCR simplified
 mas install 904280696 # Things 3
-mas install 1291898086 # Toggl Track: Gestion du temps
 mas install 497799835 # Xcode
 
 mas upgrade
