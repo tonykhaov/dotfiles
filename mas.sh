cf49d3ac544f5535dec612220fbd327287af60fe
Tony Khaov <tony.khaov@gmail.com>
2023-01-20 15:48:05 +0700

    mas > remove goodnotes and notability from installation

--- mas.sh
+++ mas.sh
@@ -7,12 +7,10 @@ echo "Install Mac App Store apps"
 mas install 1091189122 # Bear
 mas install 1287239339 # ColorSlurp
 mas install 975937182 # Fantastical - Calendar & Tasks
-mas install 1444383602 # GoodNotes 5
 mas install 430798174 # HazeOver • Distraction Dimmer™
 mas install 288545208 # Instapaper
 mas install 409183694 # Keynote
 mas install 409203825 # Numbers
-mas install 360593530  # Notability
 mas install 409201541 # Pages
 mas install 1233965871 # ScreenBrush
 mas install 1176895641 # Spark - Email App By Readdle
