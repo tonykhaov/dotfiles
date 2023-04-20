83108adb6558233b707ea629ffa941d0b93a87eb
Tony Khaov <tony.khaov@gmail.com>
2023-04-20 23:12:01 +0700

    use tailwind instead of uno

--- Lab/react_lab/src/main.tsx
+++ Lab/react_lab/src/main.tsx
@@ -2,13 +2,11 @@ import * as React from 'react'
 import { createRoot } from 'react-dom/client'
 import AppProviders from '@src/context/app-providers'
 import App from '@src/App'
-import 'uno.css'
-import '@unocss/reset/tailwind.css'
 import { worker } from '@src/utils/mocks/browser'
+import './tailwind.css'
 
 if (process.env.NODE_ENV === 'development') {
-  // UNCOMMENT IF YOU WANT TO MOCK YOUR REQUESTS IN DEVELOPMENT MODE
-  // worker.start()
+  worker.start()
 }
 
 const container = document.getElementById('root') as HTMLElement
--- Lab/react_lab/vite.config.ts
+++ Lab/react_lab/vite.config.ts
@@ -3,12 +3,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import Unocss from 'unocss/vite'
-import presetWind from '@unocss/preset-wind'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Unocss({ presets: [presetWind()] }), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
   // vitest config
   test: {
     // https://vitest.dev/config/
