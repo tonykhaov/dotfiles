37dd1b7123fd0718a562d85c00797052a04dbc3a
Tony Khaov <tony.khaov@gmail.com>
2023-04-20 22:59:52 +0700

    remove port

--- Lab/react_lab/vite.config.ts
+++ Lab/react_lab/vite.config.ts
@@ -8,14 +8,9 @@ import presetWind from '@unocss/preset-wind'
 
 import 'dotenv/config'
 
-const isDev = process.env.CI
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), Unocss({ presets: [presetWind()] }), tsconfigPaths()],
-  server: {
-    port: 3000,
-  },
   // vitest config
   test: {
     // https://vitest.dev/config/
