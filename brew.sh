f782dc77c1baf11a6b17b3610acb56254d9b869f
Tony Khaov <tony.khaov@gmail.com>
2024-01-12 17:21:25 +0100

    brew > install fonts

--- brew.sh
+++ brew.sh
@@ -60,6 +60,17 @@ setup_docker() {
   colima start
 }
 
+install_fonts() {
+  brew tap homebrew/cask-fonts
+  brew install homebrew/cask-fonts/font-ubuntu
+  brew install homebrew/cask-fonts/font-poppins
+  brew install homebrew/cask-fonts/font-open-sans
+  brew install homebrew/cask-fonts/font-roboto
+  brew install homebrew/cask-fonts/font-inter
+  brew install homebrew/cask-fonts/font-lato
+  brew install homebrew/cask-fonts/font-noto-sans
+}
+
 main() {
   install_additional_homebrew_formulaes
   load_third_party_cask_drivers
