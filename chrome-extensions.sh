3796de7c2d6118abe55a1e9d0756183fe751add5
Tony Khaov <tony.khaov@gmail.com>
2024-04-02 20:39:11 +0200

    chrome > delete deprecated extensions

--- chrome-extensions.sh
+++ chrome-extensions.sh
@@ -15,7 +15,5 @@ open https://chrome.google.com/webstore/detail/news-feed-eradicator/fjcldmjmjhkk
 open https://chrome.google.com/webstore/detail/df-tube-distraction-free/mjdepdfccjgcndkmemponafgioodelna
 open https://chrome.google.com/webstore/detail/refined-github/hlepfoohegkhhmjieoechaddaejaokhf/related
 open https://chrome.google.com/webstore/detail/disconnect/jeoacafpbcihiomhlakheieifhpjdfeo
-open https://chrome.google.com/webstore/detail/refined-twitter-lite/adhbafkkfbonbogdlaebnoegpoogngcc/related
 open https://chrome.google.com/webstore/detail/remove-w3schools/gohnadkcefpdhblajddfnhapimpdjkje/related
-open https://chrome.google.com/webstore/detail/twitch-adblock/ljhnljhabgjcihjoihakgdiicdjncpkd/related
 open https://chrome.google.com/webstore/detail/clearurls/lckanjgmijmafbedllaakclkaicjfmnk/related
