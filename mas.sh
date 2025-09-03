84900697e68edd6cf928c6b87a6d513a039fb000
Tony Khaov <tony.khaov@gmail.com>
2025-09-03 09:34:36 +0200

    mas > remove some software

--- mas.sh
+++ mas.sh
@@ -4,15 +4,10 @@ echo "Install mas cli"
 brew install mas
 
 echo "Install Mac App Store apps"
-mas install 1091189122 # Bear
-mas install 975937182 # Fantastical - Calendar & Tasks
 mas install 430798174 # HazeOver • Distraction Dimmer™
 mas install 409183694 # Keynote
 mas install 409203825 # Numbers
 mas install 409201541 # Pages
-mas install 568494494 # Pocket
-mas install 1233965871 # ScreenBrush
-mas install 1528890965 # TextSniper - OCR simplified
 mas install 904280696 # Things 3
 mas install 497799835 # Xcode
 
