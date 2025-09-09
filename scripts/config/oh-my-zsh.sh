0a41c90c9acf07a8a94a900d5a69b3e3b755b2e7
Tony Khaov <tony.khaov@gmail.com>
2025-09-09 17:48:02 +0200

    oh-my-zsh > update script

--- scripts/config/oh-my-zsh.sh
+++ scripts/config/oh-my-zsh.sh
@@ -1,25 +1,45 @@
 #!/usr/bin/env bash
 
-echo "Install oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install oh-my-zsh if not already installed
+if [ ! -d ~/.oh-my-zsh ]; then
+    echo "Installing oh-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+    echo "oh-my-zsh is already installed."
+fi
 
-echo "Import zsh config"
-rm -rf ~/.zshrc
-ln -s ~/Documents/Coding/dotfiles/.zshrc ~/.zshrc
-rm -rf ~/.zprofile
-ln -s ~/Documents/Coding/dotfiles/.profile ~/.zprofile
-
-echo "Install z"
+# Install z tool
+echo "Installing z..."
 brew install z
 
-echo "Install zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+# Install zsh plugins if not already present
+if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+    echo "Installing zsh-autosuggestions..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+else
+    echo "zsh-autosuggestions is already installed."
+fi
 
-echo "Install zsh-syntax-highlighting"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+    echo "Installing zsh-syntax-highlighting..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+else
+    echo "zsh-syntax-highlighting is already installed."
+fi
 
-echo "Install you-should-use"
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ~/.oh-my-zsh/custom/plugins/you-should-use
+if [ ! -d ~/.oh-my-zsh/custom/plugins/you-should-use ]; then
+    echo "Installing you-should-use..."
+    git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ~/.oh-my-zsh/custom/plugins/you-should-use
+else
+    echo "you-should-use is already installed."
+fi
 
-echo "Reload zsh"
-source ~/.zshrc
+# Install zsh-completions for better completion
+if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-completions ]; then
+    echo "Installing zsh-completions..."
+    git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+else
+    echo "zsh-completions is already installed."
+fi
+
+echo "oh-my-zsh setup complete."
