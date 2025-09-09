e379ae35617a25c4dc7a3fb062b9309e5c065e17
Tony Khaov <tony.khaov@gmail.com>
2025-09-09 19:32:42 +0200

    mas > remove things app

--- scripts/install/mas.sh
+++ scripts/install/mas.sh
@@ -7,7 +7,6 @@ echo "Install Mac App Store apps"
 mas install 409183694 # Keynote
 mas install 409203825 # Numbers
 mas install 409201541 # Pages
-mas install 904280696 # Things 3
 mas install 497799835 # Xcode
 
 mas upgrade
